606a31a70d211c4955e26d88c84f9c5732c6c8c7
brylie_baird <bryliebaird@gmail.com>
2021-12-02 20:31:16 -0600

    navbar collapse button started, navbar styles started

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//alert("javascript is running for ATLA website");
+const navBtn = document.querySelector("#nav-btn");
+const navList = document.querySelector(".nav-list");
+
+
+navBtn.addEventListener("click", function(){
+    if(navList.classList.contains("hidden")){
+        navList.classList.remove("hidden");
+    
+    } else if(!navList.classList.contains("hidden")){
+        console.log("howdy");
+        navList.classList.add("hidden");
+    }
+})
